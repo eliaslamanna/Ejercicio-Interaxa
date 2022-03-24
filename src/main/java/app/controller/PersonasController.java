f80c074a31b2d540bde86c80b433765f3e5f66f9
eliaslamanna <eliaslamanna@hotmail.com>
2022-03-24 15:06:19 -0300

    fix

--- src/main/java/app/controller/PersonasController.java
+++ src/main/java/app/controller/PersonasController.java
@@ -10,7 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = {"http://localhost:3000"})
+@CrossOrigin(origins = {"http://amb-app-postgress.herokuapp.com"})
 @RestController
 @RequestMapping(path = "/personas")
 public class PersonasController {
